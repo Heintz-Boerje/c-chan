12bcc796e28ed7fd5163481ead1edf6203f86aec
Heyntz Boerje <heintzrboerje@proton.me>
2023-02-08 17:00:02 -0500

    adjustments

--- c-chan/packages/qtile.scm
+++ c-chan/packages/qtile.scm
@@ -1,18 +1,29 @@
 (define-module (c-chan packages qtile)
   #:use-module (guix packages)
-  #:use-module (gnu packages base)
   #:use-module (guix download)
-  #:use-module (guix build-system python)
-  #:use-module (guix licenses)
   #:use-module (gnu packages wm))
+  
+  ; #:use-module (gnu packages base)
+  ; #:use-module (guix build-system python)
+  ; #:use-module (guix licenses)
 
 (define-public qtile
-(package
-    (inherit qtile)
-    (version "0.22.1")
-    (source (origin
-              (method url-fetch)
-              (uri (pypi-uri "qtile" version))
-              (sha256
-               (base32
-                "12wg8y33xgb0x0gd9xfylxss97p97dy1cy52yln64493fi6wphr7"))))))
+  (package
+      (inherit qtile)
+      (version "0.22.1")
+      (source (origin
+                (method url-fetch)
+                (uri (pypi-uri "qtile" version))
+                (sha256
+                 (base32
+                  "12wg8y33xgb0x0gd9xfylxss97p97dy1cy52yln64493fi6wphr7")
+          
+         )
+     
+    )
+   
+  )
+  
+ )
+ 
+)
