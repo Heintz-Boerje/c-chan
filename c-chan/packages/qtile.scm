5e1a292d9852df93a27383c2cda7851c1e842b80
Heyntz Boerje <heintzrboerje@proton.me>
2023-02-08 22:22:47 -0500

    I hope this works this time

---
+++ c-chan/packages/qtile.scm
@@ -0,0 +1,17 @@
+(use-modules (gnu packages wm)
+              (guix packages)
+              (guix download)
+              (guix build-system python)
+)
+
+(define qtile-0.22.1
+  (package
+    (inherit qtile)
+    (version "0.22.1")
+    (source (origin
+              (method url-fetch)
+              (uri (pypi-uri "qtile" version))
+              (sha256
+             (base32
+              "12wg8y33xgb0x0gd9xfylxss97p97dy1cy52yln64493fi6wphr7"))
+              ))))
