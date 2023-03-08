3fc38762da99f474669aaaf26016105f880ed3cf
heintzboerje <103048965+heintzboerje@users.noreply.github.com>
2023-03-08 15:56:42 -0500

    Create qtile-git.scm
    git version of qtile

---
+++ c-chan/packages/qtile-git.scm
@@ -0,0 +1,20 @@
+(define-module (c-chan packages qtile-git)
+  #:use-module (gnu packages wm)
+  #:use-module (guix git-download)
+  #:use-module  (guix packages)
+  #:use-module  (guix download)
+  #:use-module  (guix build-system python))
+
+(define-public qtile-git
+  (package
+    (inherit qtile)
+    (version "0.22.2")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                        (url "https://github.com/qtile/qtile.git")
+                        (commit "4c69fca361bcb1d9753b37b9b903af32b27983c5")))
+              (sha256
+             (base32
+              "12wg8y33xgb0x0gd9xfylxss97p97dy1cy52yln64493fi6wphr7"))
+              ))))
