ca1e393fc0b03c615e8186b67afc668dfa83416d
heintzboerje <103048965+heintzboerje@users.noreply.github.com>
2023-12-27 19:38:16 +0100

    Update qtile

--- c-chan/packages/qtile-22.scm
+++ c-chan/packages/qtile-22.scm
@@ -1,17 +1,17 @@
-(define-module (c-chan packages qtile-22)
+(define-module (c-chan packages qtile-23)
   #:use-module (gnu packages wm)
   #:use-module  (guix packages)
   #:use-module  (guix download)
   #:use-module  (guix build-system python))
 
-(define-public qtile-0.22.1
+(define-public qtile-0.23.0
   (package
     (inherit qtile)
-    (version "0.22.1")
+    (version "0.23.0")
     (source (origin
               (method url-fetch)
               (uri (pypi-uri "qtile" version))
               (sha256
              (base32
-              "12wg8y33xgb0x0gd9xfylxss97p97dy1cy52yln64493fi6wphr7"))
+              "1v8rxm2xg2igxv6gwa78wrkxzgfxmxfgflbjdp4fm7cxjdx3zrpa"))
               ))))
