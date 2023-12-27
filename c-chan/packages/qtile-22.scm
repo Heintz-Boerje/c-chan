5e00d19e0aa7b9658c4b685024a6480718976e86
heintzboerje <103048965+heintzboerje@users.noreply.github.com>
2023-12-27 21:21:08 +0100

    trying to update qtile

--- c-chan/packages/qtile-22.scm
+++ c-chan/packages/qtile-22.scm
@@ -4,14 +4,32 @@
   #:use-module  (guix download)
   #:use-module  (guix build-system python))
 
-(define-public qtile-0.23.0
-  (package
-    (inherit qtile)
-    (version "0.23.0")
-    (source (origin
-              (method url-fetch)
-              (uri (pypi-uri "qtile" version))
-              (sha256
-             (base32
-              "1v8rxm2xg2igxv6gwa78wrkxzgfxmxfgflbjdp4fm7cxjdx3zrpa"))
-              ))))
+(package
+  (name "python-qtile")
+  (version "0.23.0")
+  (source
+   (origin
+     (method url-fetch)
+     (uri (pypi-uri "qtile" version))
+     (sha256
+      (base32 "1v8rxm2xg2igxv6gwa78wrkxzgfxmxfgflbjdp4fm7cxjdx3zrpa"))))
+  (build-system pyproject-build-system)
+  (propagated-inputs (list python-cairocffi python-cffi python-xcffib))
+  (native-inputs (list python-bowler python-dbus-next python-pygobject
+                       python-pytest))
+  (home-page "")
+  (synopsis "A pure-Python tiling window manager.")
+  (description "This package provides a pure-Python tiling window manager.")
+  (license license:expat))
+
+;(define-public qtile-0.23.0
+ ; (package
+  ;  (inherit qtile)
+   ; (version "0.23.0")
+    ;(source (origin
+     ;         (method url-fetch)
+      ;        (uri (pypi-uri "qtile" version))
+       ;       (sha256
+        ;     (base32
+         ;     "1v8rxm2xg2igxv6gwa78wrkxzgfxmxfgflbjdp4fm7cxjdx3zrpa"))
+          ;    ))))
