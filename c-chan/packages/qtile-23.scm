113d9d0a741d546a819e2f1ec568a6129ef5e12b
heintz <heintzrboerje@proton.me>
2023-12-30 15:18:45 +0100

    testing the source of the issue

--- c-chan/packages/qtile-23.scm
+++ c-chan/packages/qtile-23.scm
@@ -4,15 +4,34 @@
   #:use-module  (guix download)
   #:use-module  (guix build-system python))
 
-( package
-  (name "qtile-23")
-  (version "0.23.0")
+;; ( package
+;;   (name "qtile-23")
+;;   (version "0.23.0")
+;;   (source
+;;    (origin
+;;      (method url-fetch)
+;;      (uri (pypi-uri "qtile" version))
+;;      (sha256
+;;       (base32 "1v8rxm2xg2igxv6gwa78wrkxzgfxmxfgflbjdp4fm7cxjdx3zrpa"))))
+;;   (build-system pyproject-build-system)
+;;   (propagated-inputs (list python-cairocffi python-cffi python-xcffib))
+;;   (native-inputs (list python-bowler python-dbus-next python-pygobject
+;;                        python-pytest))
+;;   (home-page "")
+;;   (synopsis "A pure-Python tiling window manager.")
+;;   (description "This package provides a pure-Python tiling window manager.")
+;;   (license license:expat)
+;;  )
+
+(package
+  (name "python-qtile")
+  (version "0.22.1")
   (source
    (origin
      (method url-fetch)
      (uri (pypi-uri "qtile" version))
      (sha256
-      (base32 "1v8rxm2xg2igxv6gwa78wrkxzgfxmxfgflbjdp4fm7cxjdx3zrpa"))))
+      (base32 "12wg8y33xgb0x0gd9xfylxss97p97dy1cy52yln64493fi6wphr7"))))
   (build-system pyproject-build-system)
   (propagated-inputs (list python-cairocffi python-cffi python-xcffib))
   (native-inputs (list python-bowler python-dbus-next python-pygobject
@@ -20,17 +39,3 @@
   (home-page "")
   (synopsis "A pure-Python tiling window manager.")
   (description "This package provides a pure-Python tiling window manager.")
-  (license license:expat)
- )
-
-;; (define-public qtile-0.23.0
-;;   (package
-;;    (inherit qtile)
-;;    (version "0.23.0")
-;;    (source (origin
-;;             (method url-fetch)
-;;             (uri (pypi-uri "qtile" version))
-;;             (sha256
-;;              (base32
-;;               "1v8rxm2xg2igxv6gwa78wrkxzgfxmxfgflbjdp4fm7cxjdx3zrpa"))
-;;             ))))
